75e5378aa39beba18a009e3c9c59a0824a13dcac
Richard Liu <richardadesigner@gmail.com>
2025-07-20 11:38:35 -0700

    upload api server

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def index():
+  return "<h1>View source code API</h1>"
+
+@app.route('/api', methods=['GET'])
+def api():
+  url = request.args.get('site')
+  try:
+    data = requests.get(url)
+    return jsonify({"html":data.text})
+  except Exception as e:
+    return "Error:" + str(e), 400
+
+if __name__ == "__main__":
+  port = int (os.environ.get("PORT", 5000))
+  app.run(host='0.0.0.0', port=port)
