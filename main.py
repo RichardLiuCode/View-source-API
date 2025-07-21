c566bab254a08ea86c86f08ae1ee55c1fd92509d
Richard Liu <richardadesigner@gmail.com>
2025-07-21 15:05:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,21 @@ def index():
 @app.route('/api', methods=['GET'])
 def api():
   url = request.args.get('site')
-  try:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-CN;q=0.5"
-    }
-    data = requests.get(url, headers=headers)
-    return jsonify({"html":data.text})
-  except Exception as e:
-    return jsonify({"html":"","error": str(e)}), 400
+  if url == "":
+    return jsonify({"html":"","error":"Please provide the URL"}), 400
+  elif url == "about:blank":
+    return jsonify({"html":""})
+  else:
+    try:
+      headers = {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-CN;q=0.5"
+      }
+      data = requests.get(url, headers=headers)
+      return jsonify({"html":data.text})
+    except Exception as e:
+      return jsonify({"html":"","error": str(e)}), 400
 
 if __name__ == "__main__":
   port = int (os.environ.get("PORT", 5000))
